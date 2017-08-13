8ae89c6b7444bc82593996a8189d1f41797a7a2d
drohan898 <drohan940@gmail.com>
2017-08-13 19:30:16 +0530

    Revert "TeleService: Switch telephony-ext to a static library"
    This reverts commit acbae6288b82078b7648a837c81684e1985ccd30.

--- Android.mk
+++ Android.mk
@@ -9,13 +9,12 @@ phone_common_dir := ../../apps/PhoneCommon
 src_dirs := src $(phone_common_dir)/src sip/src
 res_dirs := res $(phone_common_dir)/res sip/res
 
-LOCAL_JAVA_LIBRARIES := telephony-common voip-common ims-common
+LOCAL_JAVA_LIBRARIES := telephony-common voip-common ims-common telephony-ext
 LOCAL_STATIC_JAVA_LIBRARIES := \
+        ims-ext-common \
         org.apache.http.legacy \
         guava \
-        telephony-ext \
-        ims-ext-common \
-        volley \
+        volley
 
 LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs))
 LOCAL_SRC_FILES += \
